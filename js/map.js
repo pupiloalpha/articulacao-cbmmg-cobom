5ef706db36d9fb2adccd41e271b2a2c2e1b0bf4b
Paulo E S Mesquita <pesmesquita@gmail.com>
2026-09-04 16:48:10 -0300

    Update Leaflet icon URLs and remove default method
    Remove default icon URL method and update icon URLs to use a reliable CDN.

--- js/map.js
+++ js/map.js
@@ -64,6 +64,16 @@ function initMap() {
         zoomControl: false
     });
 
+    // Remove os caminhos padrão defeituosos
+delete L.Icon.Default.prototype._getIconUrl;
+
+// Força o recarregamento dos ícones a partir de uma CDN confiável
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon-2x.png',
+    iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png',
+});
+    
     L.control.zoom({ position: 'topright' }).addTo(map);
     
     L.Control.ZoomExtended = L.Control.extend({
